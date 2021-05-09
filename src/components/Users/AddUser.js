cc6716eb73e3dd6451875d0799f2206ba5c96d19
ChocoMilkTea <hongjun87@gmail.com>
2021-05-10 00:06:15 +0800

    initial push

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,10 @@
+const AddUser = (props) => {
+  return (
+    <form>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text' />
+    </form>
+  );
+}
+
+export default AddUser;
